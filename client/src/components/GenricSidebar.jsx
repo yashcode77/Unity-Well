19c5177bc7a4f23e805cedf10c8042b54d4bb203
Yash <yashchhaproo@gmail.com>
2024-02-20 16:56:43 +0530

    Activities and Journal button added in sidebar

--- client/src/components/GenricSidebar.jsx
+++ client/src/components/GenricSidebar.jsx
@@ -1,114 +1,132 @@
-import { Sidebar } from 'flowbite-react';
+import { Sidebar } from "flowbite-react";
 import {
-    HiUser,
-    HiArrowSmRight,
-    HiDocumentText,
-    HiOutlineUserGroup,
-    HiAnnotation,
-    HiChartPie,
-} from 'react-icons/hi';
-import { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { signoutSuccess } from '../redux/user/userSlice';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
+  HiUser,
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiAnnotation,
+  HiChartPie,
+} from "react-icons/hi";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function GenricSidebar() {
-    const location = useLocation();
-    const dispatch = useDispatch();
-    const { currentUser } = useSelector((state) => state.user);
-    const [tab, setTab] = useState('');
-    useEffect(() => {
-        const urlParams = new URLSearchParams(location.search);
-        const tabFromUrl = urlParams.get('tab');
-        if (tabFromUrl) {
-            setTab(tabFromUrl);
-        }
-    }, [location.search]);
-    const handleSignout = async () => {
-        try {
-            const res = await fetch('/api/user/signout', {
-                method: 'POST',
-            });
-            const data = await res.json();
-            if (!res.ok) {
-                console.log(data.message);
-            } else {
-                dispatch(signoutSuccess());
-            }
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
-    return (
-        <Sidebar className='w-full md:w-56 mt-5'>
-            <Sidebar.Items>
-                <Sidebar.ItemGroup className='flex flex-col gap-1 '>
-                    {currentUser && (
-                        <Link to='/dashboard?tab=dash'>
-                            <Sidebar.Item
-                                active={tab === 'dash' || !tab}
-                                icon={HiChartPie}
-                                as='div'
-                            >
-                                Dashboard
-                            </Sidebar.Item>
-                        </Link>
-                    )}
-                    <Link to='/dashboard?tab=profile'>
-                        <Sidebar.Item
-                            active={tab === 'profile'}
-                            icon={HiUser}
-                            // label={currentUser.isAdmin ? 'Admin' : 'User'}
-                            label='user'
-                            labelColor='dark'
-                            as='div'
-                        >
-                            Profile
-                        </Sidebar.Item>
-                    </Link>
-                    {currentUser && (
-                        <Link to='/dashboard?tab=posts'>
-                            <Sidebar.Item
-                                active={tab === 'posts'}
-                                icon={HiDocumentText}
-                                as='div'
-                            >
-                                Posts
-                            </Sidebar.Item>
-                        </Link>
-                    )}
-                    {currentUser && (
-                        <>
-                            <Link to='/dashboard?tab=users'>
-                                <Sidebar.Item
-                                    active={tab === 'users'}
-                                    icon={HiOutlineUserGroup}
-                                    as='div'
-                                >
-                                    Users
-                                </Sidebar.Item>
-                            </Link>
-                            <Link to='/dashboard?tab=comments'>
-                                <Sidebar.Item
-                                    active={tab === 'comments'}
-                                    icon={HiAnnotation}
-                                    as='div'
-                                >
-                                    Comments
-                                </Sidebar.Item>
-                            </Link>
-                        </>
-                    )}
-                    <Sidebar.Item
-                        icon={HiArrowSmRight}
-                        className='cursor-pointer'
-                        onClick={handleSignout}
-                    >
-                        Sign Out
-                    </Sidebar.Item>
-                </Sidebar.ItemGroup>
-            </Sidebar.Items>
-        </Sidebar>
-    );
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <Sidebar className="w-full md:w-56 mt-5">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup className="flex flex-col gap-1 ">
+          {currentUser && (
+            <Link to="/dashboard?tab=dash">
+              <Sidebar.Item
+                active={tab === "dash" || !tab}
+                icon={HiChartPie}
+                as="div"
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              // label={currentUser.isAdmin ? 'Admin' : 'User'}
+              label="user"
+              labelColor="dark"
+              as="div"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          {currentUser && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab === "posts"}
+                icon={HiDocumentText}
+                as="div"
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          {currentUser && (
+            <>
+              <Link to="/dashboard?tab=users">
+                <Sidebar.Item
+                  active={tab === "users"}
+                  icon={HiOutlineUserGroup}
+                  as="div"
+                >
+                  Users
+                </Sidebar.Item>
+              </Link>
+              <Link to="/dashboard?tab=comments">
+                <Sidebar.Item
+                  active={tab === "comments"}
+                  icon={HiAnnotation}
+                  as="div"
+                >
+                  Comments
+                </Sidebar.Item>
+              </Link>
+            </>
+          )}
+          <Link to="/activities">
+            <Sidebar.Item
+              active={tab === "activities"}
+              icon={HiAnnotation}
+              as="div"
+            >
+              Activities
+            </Sidebar.Item>
+          </Link>
+          <Link to="/diary">
+            <Sidebar.Item
+              active={tab === "activities"}
+              icon={HiAnnotation}
+              as="div"
+            >
+              Journal
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={handleSignout}
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
 }
