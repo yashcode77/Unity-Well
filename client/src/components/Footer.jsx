4655ea0be1fa7e1d2bfb880b05dfe35b0f7d68d3
Mkaif-Qureshi <quremkaif7@gmail.com>
2024-02-20 20:23:46 +0530

    Updated footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -18,9 +18,9 @@ export default function FooterCom() {
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Sample
+                Unity
               </span>
-              App
+               Well
             </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
@@ -28,18 +28,18 @@ export default function FooterCom() {
               <Footer.Title title="About" />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href="https://www.100jsprojects.com"
+                  href="https://ves.ac.in"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Undefined
+                  Undefined!!
                 </Footer.Link>
                 <Footer.Link
                   href="/about"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Sample site
+                  Unity Well
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -47,7 +47,7 @@ export default function FooterCom() {
               <Footer.Title title="Follow us" />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href="https://www.github.com/vesit"
+                  href="https://github.com/CMPN-CODECELL/Syrus-2024-Web2-UNDEFINED"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -69,15 +69,14 @@ export default function FooterCom() {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright
             href="#"
-            by="sample's blog"
+            by="Unity Well blog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon href="#" icon={BsGithub} />
-            <Footer.Icon href="#" icon={BsDribbble} />
+            <Footer.Icon href="https://github.com/CMPN-CODECELL/Syrus-2024-Web2-UNDEFINED" icon={BsGithub} />
           </div>
         </div>
       </div>
