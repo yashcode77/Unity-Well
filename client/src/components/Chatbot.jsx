ca1fb88748d65cede174fce47e9891685f1a0789
Yash <yashchhaproo@gmail.com>
2024-03-22 16:53:36 +0530

    api keys handled

--- client/src/components/Chatbot.jsx
+++ client/src/components/Chatbot.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 5 years of experience."
